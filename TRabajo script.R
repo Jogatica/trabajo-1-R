b4ac230b6e9e77bdbe62aa5a981ed5f173ceb75d
Jogatica <163201144+Jogatica@users.noreply.github.com>
2024-03-24 12:27:23 -0300

    josefa - gatica
    Trabajo 1 "Envejecimeinto Poblacional"

---
+++ TRabajo script.R
@@ -0,0 +1,15 @@
+
+
+setwd("C:/Users/josef/Downloads/Base de datos provincia y comuna Casen 2022 SPSS.sav")
+
+
+
+library(dplyr)
+library(haven)
+
+base_CASEN <- read_spss (file = "Base de datos provincia y comuna Casen 2022 SPSS.sav")
+
+class(base_CASEN)
+dim(base_CASEN)
+names(base_CASEN)
+base_CASEN %>% glimpse()
